df22178aa50dba2aad6c92d0039d5e8b190ac003
toolflow-labs <hustxujinkang@gmail.com>
2026-06-25 09:58:34 +0800

    add mqtt check tool

---
+++ field_tools/field_mqtt_check.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""MQTT check tool"""
+
+import json
+import sys
+
+def main():
+    print("mqtt placeholder")
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
